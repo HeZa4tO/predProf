b1716396adfbe1eb263d34f068b640bb1623729e
dissemblance <tuniyanalbert69@gmail.com>
2024-02-13 16:58:22 +0300

    1234

--- PredProfExam-master/3rdTask.py
+++ PredProfExam-master/3rdTask.py
@@ -1,43 +1,27 @@
-from random import randint, shuffle
-import csv
+def search(_id_project: int, file_name: str) -> str:
+    """Фукнция поиска проекта по id."""
+    f = open(file_name, encoding="utf8")
+    f.readline()
+
+    for line in f.readlines():
+        data = line.split(',')
+        if _id_project == int(data[2]):
+            f.close()
+            return f'Проект № {_id_project} делал: {data[1]} он(а) получил(а) оценку - {data[4]}'
+
+    f.close()
+    return 'Ничего не найдено'
 
 
-def create_login(user: str) -> str:
-    data_user = user.split(' ')
-    return f'{data_user[0]}_{data_user[1][0]}{data_user[2][0]}'
+def menu():
+    """Меню программы."""
+    while True:
+        id_project = input('Введите id проекта: ')
+        if id_project == 'СТОП':
+            break
 
-
-def create_password() -> str:
-    n_num = randint(1, 3)
-    n_abc = randint(1, 3)
-    n_ABC = randint(1, 3)
-    n_char = 8 - n_abc - n_ABC - n_num
-    num = [chr(randint(48, 57)) for _ in range(n_num)]
-    abc = [chr(randint(97, 122)) for _ in range(n_abc)]
-    ABC = [chr(randint(65, 90)) for _ in range(n_ABC)]
-    chars = [chr(randint(33, 47)) for _ in range(n_char)]
-    password = num + abc + ABC + chars
-    shuffle(password)
-    return ''.join(password)
-
-
-def main():
-    with open('students.csv', encoding="utf8") as file_open_students, open('students_new.csv', 'w', encoding="utf8", newline='') as file_redact_students:
-        reader = csv.reader(file_open_students)
-        writer = csv.writer(file_redact_students)
-        students = []
-
-        header = next(reader)   # Пропускаем заголовок
-        header.extend(['login', 'password'])    # Добавление столбцов "login" и "password" в заголовок
-        students.append(header)
-
-        for row in reader:
-            row.append(create_login(row[1]))
-            row.append(create_password())
-            students.append(row)
-
-        writer.writerows(students)
+        print(search(int(id_project), 'students.csv'))
 
 
 if __name__ == '__main__':
-    main()
+    menu()
---
+++ PredProfExam-master/4rdTask.py
@@ -0,0 +1,42 @@
+from random import randint, shuffle
+import csv
+
+
+def create_login(user: str) -> str:
+    data_user = user.split(' ')
+    return f'{data_user[0]}_{data_user[1][0]}{data_user[2][0]}'
+
+
+def create_password() -> str:
+    n_num = randint(1, 3)
+    n_abc = randint(1, 3)
+    n_ABC = randint(1, 3)
+    n_char = 8 - n_abc - n_ABC - n_num
+    num = [chr(randint(48, 57)) for _ in range(n_num)]
+    abc = [chr(randint(97, 122)) for _ in range(n_abc)]
+    ABC = [chr(randint(65, 90)) for _ in range(n_ABC)]
+    chars = [chr(randint(33, 47)) for _ in range(n_char)]
+    password = num + abc + ABC + chars
+    shuffle(password)
+    return ''.join(password)
+
+
+def main():
+    with open('students.csv', encoding="utf8") as file_open_students, open('students_new.csv', 'w', encoding="utf8", newline='') as file_redact_students:
+        reader = csv.reader(file_open_students)
+        writer = csv.writer(file_redact_students)
+        students = []
+
+        header = next(reader)   # Пропускаем заголовок
+        header.extend(['login', 'password'])    # Добавление столбцов "login" и "password" в заголовок
+        students.append(header)
+
+        for row in reader:
+            row.append(create_login(row[1]))
+            row.append(create_password())
+            students.append(row)
+
+        writer.writerows(students)
+
+if __name__ == '__main__':
+    main()
